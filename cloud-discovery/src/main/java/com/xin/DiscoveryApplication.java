0fe0fbc23374785b5420a29a283b3faafb16ae4f
GakiWu <leeseenlian@hotmail.com>
2019-07-07 21:53:02 +0800

    更新discovery服务

---
+++ cloud-discovery/src/main/java/com/xin/DiscoveryApplication.java
@@ -0,0 +1,14 @@
+package com.xin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //注册服务中心(服务端)
+public class DiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class,args);
+    }
+}
