33fd9c3ade7cfff05d3b924f3750a6cd20ad1ce4
GakiWu <leeseenlian@hotmail.com>
2019-07-29 23:27:57 +0800

    format this class

--- springboot-elasticsearch/src/main/java/com/xin/ElasticApplication.java
+++ springboot-elasticsearch/src/main/java/com/xin/ElasticApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ElasticApplication {
     public static void main(String[] args) {
         System.out.println("hello, Elastic!!");
-        
         SpringApplication.run(ElasticApplication.class,args);
     }
-
-
 }
