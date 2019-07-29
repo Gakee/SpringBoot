8a6a77b38de34f42e0bd7b8df7006da4bc9ada91
Gakee Woo~ <45446780+Gakee@users.noreply.github.com>
2019-07-29 15:26:31 +0000

    Update ElasticApplication.java

--- springboot-elasticsearch/src/main/java/com/xin/ElasticApplication.java
+++ springboot-elasticsearch/src/main/java/com/xin/ElasticApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ElasticApplication {
     public static void main(String[] args) {
+        System.out.println("hello, Elastic!!");
+        
         SpringApplication.run(ElasticApplication.class,args);
     }
 
