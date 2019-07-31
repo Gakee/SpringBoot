eb7a81bb7b137fa79ecaf96e464d96017faf727b
GakiWu <leeseenlian@hotmail.com>
2019-08-01 00:30:23 +0800

    add Test class

---
+++ cloud-ui/src/main/java/com/xin/domain/Test.java
@@ -0,0 +1,41 @@
+package com.xin.domain;
+
+import java.io.Serializable;
+
+public class Test implements Serializable{
+	
+	private static final long serialVersionUid = 1L;
+	
+	private Integer id;
+	
+	private String name;
+	
+	private String test1;
+	
+	private String code;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+}
