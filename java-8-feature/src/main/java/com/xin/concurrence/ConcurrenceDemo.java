ba8c49711ef18cf080c819f7c90eb4090c21e7da
Leeseenlian <leeseenlian@hotmail.com>
2019-12-24 23:58:31 +0800

    add new module

---
+++ java-8-feature/src/main/java/com/xin/concurrence/ConcurrenceDemo.java
@@ -0,0 +1,13 @@
+package com.xin.concurrence;
+
+/**
+ * 并发编程学习了解
+ *
+ * @author Y1nzm
+ * @Date 2019/12/24
+ */
+public class ConcurrenceDemo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java-8-feature/src/main/java/com/xin/optional/OptionalDemo.java
@@ -0,0 +1,17 @@
+package com.xin.optional;
+
+/**
+ * java 8 新特性学习
+ *      optional的使用
+ *      optional是从其他Google的guava工具中引入的一个新的特性
+ *      主要用于避免空指针异常
+ *
+ * @author Y1nzm
+ * @Date 2019/12/24
+ */
+public class OptionalDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
