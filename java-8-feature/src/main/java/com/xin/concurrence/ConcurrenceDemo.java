1dafd6c2605fabed35c8524387e09fb3ddc5b643
Leeseenlian <leeseenlian@hotmail.com>
2020-03-08 11:52:17 +0800

    2020年3月8日 11:50:39 update

--- java-8-feature/src/main/java/com/xin/concurrence/ConcurrenceDemo.java
+++ java-8-feature/src/main/java/com/xin/concurrence/ConcurrenceDemo.java
@@ -1,5 +1,7 @@
 package com.xin.concurrence;
 
+import java.util.WeakHashMap;
+
 /**
  * 并发编程学习了解
  *
@@ -8,6 +10,35 @@ package com.xin.concurrence;
  */
 public class ConcurrenceDemo {
     public static void main(String[] args) {
+//        WeakReference
+        new WeakHashMap();
+//        ConcurrentHashMap<Object, Object> concurrentHashMap = new ConcurrentHashMap<>();
+        TestRunnable tr = new TestRunnable();
 
+        // main 线程中执行
+        tr.run();
+
+        /**
+         * result:
+         * execute ...
+         * threadname = main
+         * execute ...
+         * threadname = Thread-0
+         */
+
+        new Thread(tr).start();
+
+        // execute in main thread
+        new Thread(tr).run();
+    }
+}
+
+class TestRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("execute ...");
+        String threadname = Thread.currentThread().getName();
+        System.out.println("threadName = " + threadname);
     }
 }
--- java-8-feature/src/main/java/com/xin/optional/OptionalDemo.java
+++ java-8-feature/src/main/java/com/xin/optional/OptionalDemo.java
@@ -12,6 +12,7 @@ package com.xin.optional;
 public class OptionalDemo {
 
     public static void main(String[] args) {
+        System.out.println("学习开始了~~~~~~~~~~~~~~");
 
     }
 }
